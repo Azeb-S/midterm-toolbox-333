a674770085c6156fd7c8913867d29bc354ab8687
Azeb Tesfay <azbseb@gmail.com>
2026-02-05 14:41:05 -0800

    code for insertNode

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -155,6 +155,9 @@ public class Toolbox {
     if (node == null || newNode == null) {
       throw new IllegalArgumentException("Node and newNode cannot be null.");
     }
+    newNode.next = node.next;
+
+    node.next = newNode;
 
   }
 
